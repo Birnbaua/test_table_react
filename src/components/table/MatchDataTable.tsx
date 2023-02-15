d958e3c8c1dc8632e3d1875ceb4445f2e2713e21
Birnbaua <andreas.bachl@24speed.at>
2023-02-15 14:03:56 +0100

    working but with compiler errors

--- src/components/table/MatchDataTable.tsx
+++ src/components/table/MatchDataTable.tsx
@@ -31,72 +31,58 @@ interface Match{
     sets?: Array<MatchSet>
 }
 
-const cellStyle = {
-    width: "10",
-    maxWidth: 300,
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    borderStyle: "border-box",
-    whiteSpace: 'nowrap'
+interface TableProps {
+    title?: string
+}
+
+interface TableState {
+    rows?: Array<Match>
+    columnDefinition?: Array<ColumnDefinition>
 }
 
 const rowColors = ["#ABEBC6","#FAD7A0","#AED6F1","#F9E79F","#D2B4DE"]
 
-const columnDefinition: Array<ColumnDefinition> = []
 
-class MatchDataTable extends Component<Array<Match>,Array<ColumnDefinition>> {
-    constructor(props: any) {
+class MatchDataTable extends Component<TableProps,TableState> {
+    constructor(props: TableProps) {
         super(props);
     }
 
-    rows: Array<Match> = exampleData
-    
-    columnDefinition: Array<ColumnDefinition> = [
-        {field: "match_no", title: 'Spiel Nr.', widthGrow: 0.25, minWidth: 50, headerSort: false, editable: false, editor: "input",},
-        {field: "round", title: 'Runde', widthGrow: 0.25, headerSort: false, editable: false, editor: "input",},
-        {field: "start", title: 'Start', widthGrow: 0.25, headerSort: false, editable: true, editor: "input"},
-        {field: "field.no", title: 'Feld Nr.', widthGrow: 0.25, headerSort: false, editable: false, editor: "input",},
-        {field: "field.name", title: 'Feld', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
-        {field: "team_a.name", title: 'Team A', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
-        {field: "team_b.name", title: 'Team B', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
-        {field: "referee.name", title: 'Schiedsrichter', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
-    ]
+    state = {
+        rows: exampleData,
+        columnDefinition: [
+            {field: "match_no", title: 'Spiel Nr.', widthGrow: 0.25, headerSort: false, editable: false, editor: "input",},
+            {field: "round", title: 'Runde', widthGrow: 0.25, headerSort: false, editable: false, editor: "input",},
+            {field: "start", title: 'Start', widthGrow: 0.25, headerSort: false, editable: true, editor: "input"},
+            {field: "field.no", title: 'Feld Nr.', widthGrow: 0.25, headerSort: false, editable: false, editor: "input",},
+            {field: "field.name", title: 'Feld', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
+            {field: "team_a.name", title: 'Team A', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
+            {field: "team_b.name", title: 'Team B', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
+            {field: "referee.name", title: 'Schiedsrichter', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
+        ]
+    }
 
     componentDidMount(): void {
-        if(this.rows.length != 0) {
-            var sets: Array<number> = this.rows.map(row => {if(row.sets != null){return row.sets.length}else{return 0}})
+        if(this.state.rows.length != 0) {
+            var sets: Array<number> = this.state.rows.map(row => {if(row.sets != null){return row.sets.length}else{return 0}})
             var setNo: number = Math.max(...sets)
             console.log("Number of sets: " + Math.max(...sets))
-            this.columnDefinition = [
-                {field: "match_no", title: 'Spiel Nr.', widthGrow: 0.25, minWidth: 50, headerSort: false, editable: false, editor: "input",},
-                {field: "round", title: 'Runde', widthGrow: 0.25, headerSort: false, editable: false, editor: "input",},
-                {field: "start", title: 'Start', widthGrow: 0.25, headerSort: false, editable: true, editor: "input"},
-                {field: "field.no", title: 'Feld Nr.', widthGrow: 0.25, headerSort: false, editable: false, editor: "input",},
-                {field: "field.name", title: 'Feld', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
-                {field: "team_a.name", title: 'Team A', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
-                {field: "team_b.name", title: 'Team B', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
-                {field: "referee.name", title: 'Schiedsrichter', widthGrow: 0.5, headerSort: false, editable: false, editor: "input"},
-            ]
-            let arr: ColumnDefinition[] = []
             for(let i = 0;i<setNo;i++) {
-                arr.push({field: "sets."+ i +".points_a", title: 'Satz'+i+' a' , widthGrow: 0.2, headerSort: false, editable: true, editor: "input"})
-                arr.push({field: "sets."+ i +".points_b", title: 'Satz'+i+' b' , widthGrow: 0.2, headerSort: false, editable: true, editor: "input"})
+                this.state.columnDefinition.push({field: "sets."+ i +".points_a", title: 'Satz'+i+' a' , widthGrow: 0.2, headerSort: false, editable: true, editor: "input"})
+                this.state.columnDefinition.push({field: "sets."+ i +".points_b", title: 'Satz'+i+' b' , widthGrow: 0.2, headerSort: false, editable: true, editor: "input"})
             }
-            console.log(this.columnDefinition.length)
-            this.columnDefinition.push(...arr)
-            console.log(this.columnDefinition.length)
         }
     }
 
     componentWillUnmount(): void {
-        //this.columnDefinition = []
+        this.state.columnDefinition = []
     }
 
     render() : ReactNode {
         return (        
             <div>
                 <ReactTabulator 
-                    data={this.rows} columns={this.columnDefinition}
+                    data={this.state.rows} columns={this.state.columnDefinition}
                     layout={'fitColumns'}
                     rowFormatter={function(row){
                         //row - row component
